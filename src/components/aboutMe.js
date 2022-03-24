e9d126275170204689bcc72ee140413c607602bc
gokul299 <83939754+gokul299@users.noreply.github.com>
2022-03-25 01:18:10 +0530

    Update aboutMe.js

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -9,19 +9,23 @@ function AboutMe(){
         <div className="col-lg-6">
           <div className="aboutDiv">
             <p className="title1">About <span style={{color:"tomato"}}> me</span></p>
+
+           
+             
             <p className="about about2"> Hello! My name is Gokul and I enjoy creating things that live on the internet 
               and also love to solve the complex problems . My
               interest in web development started when i decided to switch my career to Developer,
               and decided to start my journey as a Full-stack
               Developer. I completed Fullstack developer program from <span className="imp"> GUVI (IIT Madaras). </span> </p>
             <p className="about about2">Fast-forward to today, I am looking for a challenging, respectable and responsible work, that can form out my personal
-              and professional growth and helps me to contribute effectively  to the company growth
-             .</p>
+              and professional growth and helps me to contribute effectively  to the company growth .</p>
+              <img src="./images/resume.png" alt="resume"/>
+              <a href="https://drive.google.com/file/d/1ZaBb4PzmpEAhyUN-LwcouPkZHfttKtfm/view?usp=sharing" target="blank" className="anchor" style={{color:"white"}}> <button type="button" class="btn btn-warning">RESUME</button></a>
           </div>
           
         </div>
         <div className="col-lg-6 aboutMeHeroImg" style={{textAlign:"center"}}>
-          <img src="./images/avatar.jpg"alt="devop" style={{width:"50%" , marginTop:"85px",borderRadius:"50%",height:"70%",border:"5px solid darkorange"}}/>
+          <img src="./images/avatar.jpg"alt="devop" style={{ marginTop:"85px",borderRadius:"50%",height:"350px",width:"350px",border:"5px solid skyblue"}}/>
         </div>
       </div>
     </div>
